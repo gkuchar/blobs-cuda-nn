31aa84199a880346766b0ee6a623493cec83e727
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-03-20 11:22:41 -0500

    softmax classifier: implemented data loading to device, W and b init, and forward pass

--- code/src/main.cu
+++ code/src/main.cu
@@ -4,5 +4,78 @@
 #include "../include/thrust_nn.h"
 
 int main(int argc, char *argv[]) {
-    return 0;
+    // 1. Load data onto device: input matrix X, target vector y
+    std::vector<float> X;
+    std::vector<int> y;
+
+    int num_entries = 0;
+    int num_parameters = 0;
+
+    bool completed = dataio::load_csv_features_labels("../data/blobs2d_3class.csv", X, y, num_entries, num_parameters);
+
+    if (!completed) {
+        printf("Error loading data");
+        exit(1);
+    }
+
+    thrust::device_vector<float> d_X = X;
+    thrust::device_vector<int> d_y = y;
+
+    // 2. Initalize hyperparameters + Weights and bias vectors
+    float learning_rate = 0.1f;
+    int num_epochs = 200;
+    int batch_size = 32;
+    int num_classes = 3;
+
+    // Host Weights and bias vectors
+    std::vector<float> h_W(num_parameters * num_classes);
+    std::vector<float> h_b(num_classes);
+
+    // Fill init with random but realistic values
+    std::random_device rd;  // Seed
+    std::mt19937 gen(rd()); // Standard generator
+    std::normal_distribution<float> dist(0.0f, 0.01f);
+
+    for (int i = 0; i < h_W.size(); i++) {
+        h_W[i] = dist(gen);
+    }
+
+    for (int i = 0; i < h_b.size(); i++) {
+        h_b[i] = dist(gen);
+    }
+
+    // Device Weights and bias vectors
+    thrust::device_vector<float> d_W = h_W;
+    thrust::device_vector<float> d_b = h_b;
+
+    // Device logits and probs vectors
+    thrust::device_vector<float> d_logits(num_classes, 0.0f);
+    thrust::device_vector<float> d_probs(num_classes, 0.0f);
+
+    // Temp row device vectors for logit computation
+    thrust::device_vector<float> d_temp_W_row(num_parameters);
+    thrust::device_vector<float> d_temp_X_row(num_parameters);
+
+    // 3. Training Loop
+    int num_batches = num_entries / batch_size;   
+    int batch_start, batch_end;
+    for (int i = 0; i < num_epochs; i++) {
+        for (int j = 0; j < num_batches; j++) {
+            batch_start = j * batch_size;
+            batch_end = min(batch_start + batch_size, num_entries);
+            for (int s = batch_start; s < batch_end; s++) {
+
+                // Calculate logit and probability of each class
+                for (int c = 0; c < num_classes; c++) {
+                    int W_row_c = c * num_parameters;
+
+                    thrust::copy(d_W.begin() + W_row_c, d_W.begin() + W_row_c + num_parameters, d_temp_W_row.begin());
+                    thrust::copy(d_X.begin() + (s * num_parameters), d_X.begin() + (s * num_parameters) + num_parameters, d_temp_X_row.begin());
+        
+                    d_logits[c] = thrustnn::dot(d_temp_W_row, d_temp_X_row) + d_b[c];
+                }
+                thrustnn::softmax(d_logits, d_probs);
+            }
+        }
+    }
 }
