d23f9223f9942792f3624bd7e7503365bdc16617
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-03-22 12:11:43 -0500

    fully implemented 1 hidden layer MLP with forward and back propogation

--- code/src/main.cu
+++ code/src/main.cu
@@ -117,11 +117,13 @@ int main(int argc, char *argv[]) {
         int predicted = max_it - d_probs.begin();
         if (predicted == d_y[s]) correct++;
     }
-    printf("Accuracy: %.2f%%\n", 100.0f * correct / num_entries);
+    float soft_max_acc = 100.0f * correct / num_entries;
+    printf("Softmax Accuracy: %.2f%%\n", soft_max_acc);
 
     // MILESTONE 2: 1 Hidden Layer MLP
     // a. Init W1, W2, b1, b2, temp hidden vector
     int hidden_layer_size = 8;
+    learning_rate = .01f;
 
     // Host Weights and bias vectors
     std::vector<float> h_W1(num_parameters * hidden_layer_size);
@@ -150,22 +152,32 @@ int main(int argc, char *argv[]) {
     thrust::device_vector<float> d_b1 = h_b1;
     thrust::device_vector<float> d_b2 = h_b2;
     thrust::device_vector<float> d_hidden(hidden_layer_size, 0.0f);
+    thrust::device_vector<float> d_hidden_vals(hidden_layer_size, 0.0f);
+    thrust::device_vector<float> d_error(num_classes, 0.0f);
 
-    // b. Forward pass
+    thrust::device_vector<float> d_temp_W1_row(num_parameters);
+    thrust::device_vector<float> d_temp_X1_row(num_parameters);
+    thrust::device_vector<float> d_temp_W2_row(hidden_layer_size);
+    thrust::device_vector<float> d_temp_X2_row(hidden_layer_size);
+
+    // b. Training Loop
     for (int i = 0; i < num_epochs; i++) {
+        cost = 0;
         for (int j = 0; j < num_batches; j++) {
             batch_start = j * batch_size;
             batch_end = min(batch_start + batch_size, num_entries);
             for (int s = batch_start; s < batch_end; s++) {
 
+                // Forward Propogation
                 // Hidden vector = W1*X + b1
                 for (int h = 0; h < hidden_layer_size; h++) {
                     W_row_h = h * num_parameters;
 
-                    thrust::copy(d_W1.begin() + W_row_h, d_W1.begin() + W_row_h + num_parameters, d_temp_W_row.begin());
-                    thrust::copy(d_X.begin() + (s * num_parameters), d_X.begin() + (s * num_parameters) + num_parameters, d_temp_X_row.begin());
+                    thrust::copy(d_W1.begin() + W_row_h, d_W1.begin() + W_row_h + num_parameters, d_temp_W1_row.begin());
+                    thrust::copy(d_X.begin() + (s * num_parameters), d_X.begin() + (s * num_parameters) + num_parameters, d_temp_X1_row.begin());
         
-                    d_hidden[h] = thrustnn::dot(d_temp_W_row, d_temp_X_row) + d_b1[h];
+                    d_hidden[h] = thrustnn::dot(d_temp_W1_row, d_temp_X1_row) + d_b1[h];
+                    d_hidden_vals[h] = d_hidden[h];
                 }
 
                 // ReLU
@@ -175,15 +187,96 @@ int main(int argc, char *argv[]) {
                 for (int c = 0; c < num_classes; c++) {
                     W_row_c = c * hidden_layer_size;
 
-                    thrust::copy(d_W2.begin() + W_row_c, d_W2.begin() + W_row_c + hidden_layer_size, d_temp_W_row.begin());
-                    thrust::copy(d_hidden.begin(), d_hidden.begin() + hidden_layer_size, d_temp_X_row.begin());
+                    thrust::copy(d_W2.begin() + W_row_c, d_W2.begin() + W_row_c + hidden_layer_size, d_temp_W2_row.begin());
+                    thrust::copy(d_hidden.begin(), d_hidden.begin() + hidden_layer_size, d_temp_X2_row.begin());
         
-                    d_logits[c] = thrustnn::dot(d_temp_W_row, d_temp_X_row) + d_b2[c];
+                    d_logits[c] = thrustnn::dot(d_temp_W2_row, d_temp_X2_row) + d_b2[c];
                 }
 
                 // Softmax
                 thrustnn::softmax(d_logits, d_probs);
+
+                loss = thrustnn::cross_entropy_one(d_probs, d_y[s]);
+                cost += loss;
+
+                // Backpropogation (W/b updating)
+                // Compute output layer error, update row W2 and b2
+                for (int c = 0; c < num_classes; c++) {
+                    W_row_c = c * hidden_layer_size;
+
+                    thrust::copy(d_W2.begin() + W_row_c, d_W2.begin() + W_row_c + hidden_layer_size, d_temp_W2_row.begin());
+                    thrust::copy(d_hidden.begin(), d_hidden.end(), d_temp_X2_row.begin());
+                    error_c = (float)d_probs[c] - (c == d_y[s] ? 1 : 0);
+                    d_error[c] = error_c;
+                    thrustnn::saxpy(d_temp_W2_row, d_temp_X2_row, -learning_rate * error_c);
+                    thrust::copy(d_temp_W2_row.begin(), d_temp_W2_row.end(), d_W2.begin() + W_row_c);
+                    d_b2[c] = d_b2[c] - learning_rate * error_c;
+                }
+
+                // Update update row W1 and b1
+                float grad_h;
+                for (int h = 0; h < hidden_layer_size; h++) {
+                    grad_h = 0;
+                    for (int c = 0; c < num_classes; c++) {
+                        grad_h += d_error[c] * (float)d_W2[hidden_layer_size * c + h];     
+                    }
+                    if ((float)d_hidden_vals[h] <= 0) {
+                        grad_h = 0;
+                    }
+
+                    W_row_h = h * num_parameters;
+
+                    thrust::copy(d_W1.begin() + W_row_h, d_W1.begin() + W_row_h + num_parameters, d_temp_W1_row.begin());
+                    thrust::copy(d_X.begin() + (s * num_parameters), d_X.begin() + (s * num_parameters) + num_parameters, d_temp_X1_row.begin());
+                    thrustnn::saxpy(d_temp_W1_row, d_temp_X1_row, -learning_rate * grad_h);
+                    thrust::copy(d_temp_W1_row.begin(), d_temp_W1_row.end(), d_W1.begin() + W_row_h);
+                    d_b1[h] = d_b1[h] - learning_rate * grad_h;
+                }
+                
             }
         }
+        printf("MLP Epoch %d  loss: %.4f\n", i, cost / num_entries);
+    }
+
+    // c. Evaluation
+    correct = 0;
+    for (int s = 0; s < num_entries; s++) {
+        for (int h = 0; h < hidden_layer_size; h++) {
+            W_row_h = h * num_parameters;
+
+            thrust::copy(d_W1.begin() + W_row_h, d_W1.begin() + W_row_h + num_parameters, d_temp_W1_row.begin());
+            thrust::copy(d_X.begin() + (s * num_parameters), d_X.begin() + (s * num_parameters) + num_parameters, d_temp_X1_row.begin());
+
+            d_hidden[h] = thrustnn::dot(d_temp_W1_row, d_temp_X1_row) + d_b1[h];
+        }
+
+        // ReLU
+        thrustnn::relu_inplace(d_hidden);
+
+        // Output vector = W2*HIDDEN + b2
+        for (int c = 0; c < num_classes; c++) {
+            W_row_c = c * hidden_layer_size;
+
+            thrust::copy(d_W2.begin() + W_row_c, d_W2.begin() + W_row_c + hidden_layer_size, d_temp_W2_row.begin());
+            thrust::copy(d_hidden.begin(), d_hidden.begin() + hidden_layer_size, d_temp_X2_row.begin());
+
+            d_logits[c] = thrustnn::dot(d_temp_W2_row, d_temp_X2_row) + d_b2[c];
+        }
+
+        // Softmax
+        thrustnn::softmax(d_logits, d_probs);
+
+        auto max_it = thrust::max_element(d_probs.begin(), d_probs.end());
+        int predicted = max_it - d_probs.begin();
+        if (predicted == d_y[s]) correct++;
+    }
+    float mlp_acc = 100.0f * correct / num_entries;
+    printf("1 Hidden Layer MLP Accuracy: %.2f%%\n", mlp_acc);
+
+    if (mlp_acc >= soft_max_acc) {
+        printf("MLP was %.2f%% percent more accurate than Softmax\n", mlp_acc - soft_max_acc);
+    }
+    else {
+        printf("Softmax was %.2f%% percent more accurate than MLP\n", soft_max_acc - mlp_acc);
     }
 }
